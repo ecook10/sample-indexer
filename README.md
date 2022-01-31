9150452dbd1b9f20656a5116d5e1e4d8b3973818
Evan Cook <evancookie2@gmail.com>
2022-01-30 22:43:16 -0800

    document running

--- README.md
+++ README.md
@@ -1,3 +1,21 @@
 # Sample Indexer
 
 score + organize audio samples based on their sonic attributes
+
+# Running
+1. initiate + activate virtual env
+```
+> python -m venv .venv
+> .\.venv\Scripts\Activate.ps1
+```
+_for PowerShell - see here for other options: https://docs.python.org/3/library/venv.html_
+
+2. install dependencies
+```
+> pip install -r requirements.txt
+```
+
+3. run a script
+```
+> python load-wav.py
+```
--- load-wav.py
+++ load-wav.py
@@ -6,8 +6,10 @@ from plotly.offline import init_notebook_mode
 import plotly.graph_objs as go
 import plotly
 
-# read WAV file using scipy.io.wavfile
-fs_wav, data_wav = wavfile.read('data\cajon1.wav')
+# FILE = 'data\cajon1.wav'
+FILE = 'data\hit.wav'
 
-print('Signal Duration = {} seconds'.
-      format(data_wav.shape[0] / fs_wav))
+# read WAV file using scipy.io.wavfile
+fs_wav, data_wav = wavfile.read(FILE)
+
+print('Signal Duration = {} seconds'.format(data_wav.shape[0] / fs_wav))
