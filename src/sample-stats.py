827a6e4d33e46c41452796d5b44447619dd16051
Evan Cook <evancookie2@gmail.com>
2022-03-12 21:17:25 -0800

    runs

--- src/sample-stats.py
+++ src/sample-stats.py
@@ -1,38 +1,56 @@
 import numpy as np
 from scipy.io import wavfile
-import scipy.fftpack as scp
+from scipy.fft import fft, fftfreq
 
 # x samples / sec / 1000
 
-MILLIS_PER_CHUNK = 1.0
+MILLIS_PER_CHUNK = 1
 
-def printStats(fileName):
-    sampleRate, sampleData = wavfile.read(fileName)
+class SampleStats:
 
-    # only analyze full chunks
-    samplesPerChunk = sampleRate / 1000 * MILLIS_PER_CHUNK
-    fullChunks = sampleData.shape[0] % samplesPerChunk
-    chunks = np.reshape(sampleData[:samplesPerChunk*fullChunks], (fullChunks, -1, 2))
-    chunkRms = np.sqrt(np.sum(np.power(chunks, 2), axis=1))
-    chunkRmsMono = np.mean(chunkRms, axis=1)
+    def __init__(self, fileName):
+        sampleRate, sampleData = wavfile.read(fileName)
 
-    t = np.arange(fullChunks)
-    linFit = np.polyfit(t, chunkRmsMono, 1)
-    linError = linFit[1][0]
-    expFit = np.polyfit(t, chunkRmsMono, 2)
-    expError = expFit[1][0]
+        # only analyze full chunks
+        samplesPerChunk = sampleRate // 1000 * MILLIS_PER_CHUNK
+        fullChunks = sampleData.shape[0] % samplesPerChunk
+        chunks = np.reshape(sampleData[:samplesPerChunk*fullChunks], (fullChunks, -1, 2))
+        chunkRms = np.sqrt(np.sum(np.power(chunks, 2), axis=1))
+        chunkRmsMono = np.mean(chunkRms, axis=1)
 
-    fft = np.abs(scp.fft(sampleData))
+        t = np.arange(fullChunks)
+        linFit = np.polyfit(t, chunkRmsMono, 1, full=True)
+        expFit = np.polyfit(t, chunkRmsMono, 2, full=True)
 
-    durationSecs = sampleData.shape[0] / sampleRate
-    fundFreq = np.argmax(fft, axis=1)
-    meanFreqLevel = np.mean(fft, axis=1)
-    noiseIndex = meanFreqLevel / fft[fundFreq]
+        fftValues = np.abs(fft(sampleData))
+        fftFreqs = fftfreq(sampleData.shape[0], 1 / sampleRate)
 
-    print(f'duration (seconds): {durationSecs}')
-    print(f'linear volume decay error: {linError} (coefficients: {linError[0]}')
-    print(f'exponential volumn decay error: {expError} (coefficients: {expError[0]}')
-    print(f'fundamental frequency (Hz): {fundFreq}')
-    print(f'noise level: {noiseIndex}')
+        fundFreqIndices = np.argmax(fftValues, axis=0)
+        fundFreqs = fftFreqs[fundFreqIndices]
 
-printStats('data\hit.wav')
+        meanFftValues = np.mean(fftValues, axis=0)
+        noiseIndex = np.mean(np.array([meanFftValues[i] / fftValues[fundFreqIndices[i], i] for i in range(fundFreqIndices.size)]))
+
+        self.durationSecs = sampleData.shape[0] / sampleRate
+        self.linFit = linFit
+        self.expFit = expFit
+        self.fundFreqs = fundFreqs
+        self.noiseIndex = noiseIndex
+
+    def linError(self):
+        self.linFit[1][0]
+
+    def expError(self):
+        self.expFit[1][0]
+
+    def __str__(self):
+        return f"""
+        duration (seconds): {self.durationSecs}
+        linear volume decay error: {self.linError()} (coefficients: {self.linFit[0]}
+        exponential volumn decay error: {self.expError()} (coefficients: {self.expFit[0]}
+        fundamental frequency (Hz): {self.fundFreqs}
+        noise level: {self.noiseIndex}
+        """
+
+stats = SampleStats('data\piano-chord.wav')
+print(str(stats))
